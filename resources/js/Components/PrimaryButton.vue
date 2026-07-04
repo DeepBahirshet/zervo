c0aa7c696d8f6dc4400a2106c3e2689ac573df63
DeepBahirshet <deepbahirshet@gmail.com>
2026-07-04 14:48:24 +0530

    Setup Laravel Inertia

---
+++ resources/js/Components/PrimaryButton.vue
@@ -0,0 +1,7 @@
+<template>
+    <button
+        class="inline-flex items-center rounded-md border border-transparent bg-amber-500 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-gray-900 transition duration-150 ease-in-out hover:bg-amber-600 focus:bg-amber-600 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-offset-2 active:bg-amber-700"
+    >
+        <slot />
+    </button>
+</template>
